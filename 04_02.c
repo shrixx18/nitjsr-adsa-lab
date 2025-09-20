c17dc0ce157e3899d9fe130088e39deaa405c522
Shriverdhan Pathak <emerginggiant18@gmail.com>
2025-09-20 10:54:02 +0530

    set5

--- 04_02.c
+++ 04_02.c
@@ -3,122 +3,78 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-// A structure for a single node in the AVL tree
 struct Node {
     int key;
     struct Node *left;
     struct Node *right;
     int height;
 };
-
-// Function to get the height of a node
 int height(struct Node *N) {
     if (N == NULL)
         return 0;
     return N->height;
 }
-
-// Function to get the maximum of two integers
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
-
-// Function to create a new node
 struct Node* newNode(int key) {
     struct Node* node = (struct Node*)malloc(sizeof(struct Node));
     node->key = key;
     node->left = NULL;
     node->right = NULL;
-    node->height = 1; // New node is initially added at leaf
+    node->height = 1;
     return(node);
 }
-
-// Right rotate subtree rooted with y
 struct Node *rightRotate(struct Node *y) {
     struct Node *x = y->left;
     struct Node *T2 = x->right;
-
-    // Perform rotation
     x->right = y;
     y->left = T2;
-
-    // Update heights
     y->height = max(height(y->left), height(y->right)) + 1;
     x->height = max(height(x->left), height(x->right)) + 1;
-
-    // Return new root
     return x;
 }
-
-// Left rotate subtree rooted with x
 struct Node *leftRotate(struct Node *x) {
     struct Node *y = x->right;
     struct Node *T2 = y->left;
-
-    // Perform rotation
     y->left = x;
     x->right = T2;
-
-    // Update heights
     x->height = max(height(x->left), height(x->right)) + 1;
     y->height = max(height(y->left), height(y->right)) + 1;
-
-    // Return new root
     return y;
 }
 
-// Get Balance factor of node N
 int getBalance(struct Node *N) {
     if (N == NULL)
         return 0;
     return height(N->left) - height(N->right);
 }
 
-// Function to insert a key into the AVL tree
 struct Node* insertItem(struct Node* node, int key) {
-    // 1. Perform the normal BST insertion
     if (node == NULL)
         return(newNode(key));
-
     if (key < node->key)
         node->left = insertItem(node->left, key);
     else if (key > node->key)
         node->right = insertItem(node->right, key);
-    else // Equal keys are not allowed in BST
+    else 
         return node;
-
-    // 2. Update height of this ancestor node
     node->height = 1 + max(height(node->left), height(node->right));
-
-    // 3. Get the balance factor and check for imbalance
     int balance = getBalance(node);
-
-    // Left Left Case
     if (balance > 1 && key < node->left->key)
         return rightRotate(node);
-
-    // Right Right Case
     if (balance < -1 && key > node->right->key)
         return leftRotate(node);
-
-    // Left Right Case
     if (balance > 1 && key > node->left->key) {
         node->left = leftRotate(node->left);
         return rightRotate(node);
     }
-
-    // Right Left Case
     if (balance < -1 && key < node->right->key) {
         node->right = rightRotate(node->right);
         return leftRotate(node);
     }
-
-    // Return the (unchanged) node pointer
     return node;
 }
-
-// Function to find the node with the minimum value
 struct Node * minValueNode(struct Node* node) {
     struct Node* current = node;
     while (current->left != NULL)
@@ -126,9 +82,8 @@ struct Node * minValueNode(struct Node* node) {
     return current;
 }
 
-// Function to delete a key from the AVL tree
 struct Node* deleteItem(struct Node* root, int key) {
-    // 1. Perform standard BST deletion
+
     if (root == NULL)
         return root;
 
@@ -136,7 +91,7 @@ struct Node* deleteItem(struct Node* root, int key) {
         root->left = deleteItem(root->left, key);
     else if (key > root->key)
         root->right = deleteItem(root->right, key);
-    else { // Node with key to be deleted is found
+    else { 
         if ((root->left == NULL) || (root->right == NULL)) {
             struct Node *temp = root->left ? root->left : root->right;
             if (temp == NULL) {
@@ -155,17 +110,14 @@ struct Node* deleteItem(struct Node* root, int key) {
     if (root == NULL)
         return root;
 
-    // 2. Update height of the current node
+   
     root->height = 1 + max(height(root->left), height(root->right));
 
-    // 3. Get the balance factor and rebalance if needed
     int balance = getBalance(root);
 
-    // Left Left Case
     if (balance > 1 && getBalance(root->left) >= 0)
         return rightRotate(root);
 
-    // Left Right Case
     if (balance > 1 && getBalance(root->left) < 0) {
         root->left = leftRotate(root->left);
         return rightRotate(root);
